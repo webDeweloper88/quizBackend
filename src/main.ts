42c7aab3a8aca1580c18f11e6328e2ba05553180
Makhmud <m.xudoyberdiev88@gmail.com>
2024-09-07 08:52:13 +0500

    feat: добавлен ValidationPipe и применен в main.ts
    - Добавлен глобальный ValidationPipe для валидации входящих данных.
    - Валидация теперь включена на уровне всего приложения через файл main.ts.

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { AppModule } from '@modules/app/app.module';
+import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
+  app.useGlobalPipes(new ValidationPipe());
   const port = configService.get('port');
   await app.listen(port);
 }
