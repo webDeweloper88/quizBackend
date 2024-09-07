d2d714a5da381accd4de6cb75096fef3b00747d8
Makhmud <m.xudoyberdiev88@gmail.com>
2024-09-07 08:56:21 +0500

    feat: установлена и настроена документация API через Swagger
    - Установлены зависимости @nestjs/swagger и swagger-ui-express.
    - Настроен SwaggerModule в main.ts для генерации документации API.
    - Документация доступна по адресу /api

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,21 @@ import { AppModule } from '@modules/app/app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
   app.useGlobalPipes(new ValidationPipe());
+
+  // Конфигурация Swagger
+  const config = new DocumentBuilder()
+    .setTitle('API проекта')
+    .setDescription('Документация API для проекта')
+    .setVersion('1.0')
+    .addBearerAuth() // Для добавления авторизации через Bearer Token
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
   const port = configService.get('port');
   await app.listen(port);
 }
