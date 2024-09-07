e1a595ac4894cb895c530a854796de791fc361f4
Makhmud <m.xudoyberdiev88@gmail.com>
2024-09-07 08:48:43 +0500

    feat: добавлена поддержка @nestjs/config для работы с переменными окружения
    - Установлены зависимости @nestjs/config и dotenv.
    - Настроен глобальный доступ к переменным окружения через ConfigModule.
    - Добавлен пример использования ConfigService для получения конфигураций (port).
    - Добавлен файл `configurations.ts` для централизованной конфигурации приложения.

---
+++ src/config/configurations.ts
@@ -0,0 +1,14 @@
+export default () => ({
+  port: parseInt(process.env.APP_PORT, 10) || 3000,
+  database: {
+    DB_HOST: process.env.DB_HOST,
+    DB_PORT: parseInt(process.env.DB_PORT, 10) || 5432,
+    DB_USER: process.env.DB_USER,
+    DB_PASSWORD: process.env.DB_PASSWORD,
+    DB_NAME: process.env.DB_NAME,
+  },
+  jwt: {
+    JWT_SECRET: process.env.JWT_SECRET,
+    JWT_EXPIRES_IN: process.env.JWT_EXPIRES_IN || '3600',
+  },
+});
--- src/modules/app/app.module.ts
+++ src/modules/app/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import configurations from '@config/configurations';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [configurations],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
